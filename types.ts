ebd7346490285003c809c16f67f474a04715d2f1
GessicaGobbi <gessicagobbi@hotmail.com>
2025-05-13 11:54:13 -0300

    produto

--- types.ts
+++ types.ts
@@ -7,17 +7,18 @@ type Product = {
   id: string;
   name: string;
   price: number;
+  tipo: string;
 };
 
 function createPerson(name: string, age: number): Person {
   return { name, age };
 }
 
-function createProduct(id: string, name: string, price: number): Product {
-  return { id, name, price };
+function createProduct(id: string, name: string, price: number , tipo :string): Product {
+  return { id, name, price, tipo };
 }
 
 const person1 = createPerson("Anders Heljsberg", 64);
-const product1 = createProduct("LTP-041209", "Laptop", 5000);
+const product1 = createProduct("LTP-041209", "Laptop", 5000,"beleza");
 
 console.log(person1, product1);
