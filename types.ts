fde1db4c90f87f0ea043a2cf97e91b7b51904618
Jhamyllie <milli.santana2017@gmail.com>
2025-05-13 11:48:56 -0300

    Adiciona campo ao type person

--- types.ts
+++ types.ts
@@ -1,6 +1,7 @@
 type Person = {
   name: string;
   age: number;
+  sexo: string;
 };
 
 type Product = {
@@ -9,15 +10,15 @@ type Product = {
   price: number;
 };
 
-function createPerson(name: string, age: number): Person {
-  return { name, age };
+function createPerson(name: string, age: number, sexo: string): Person {
+  return { name, age, sexo };
 }
 
 function createProduct(id: string, name: string, price: number): Product {
   return { id, name, price };
 }
 
-const person1 = createPerson("Anders Heljsberg", 64);
+const person1 = createPerson("Anders Heljsberg", 64, "masculino");
 const product1 = createProduct("LTP-041209", "Laptop", 5000);
 
 console.log(person1, product1);
